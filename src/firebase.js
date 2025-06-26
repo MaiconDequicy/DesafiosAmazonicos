b7234e62d4e4fb947196086f0645edd15dbdcee8
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2025-06-26 19:56:10 -0400

    adicao do db para a utilizacao do firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Adicione esta linha
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCWBbf016WqjK32LWsxuHxx0e6P5QvWcyI",
   authDomain: "desafiosicos.firebaseapp.com",
@@ -16,6 +12,8 @@ const firebaseConfig = {
   measurementId: "G-PRDM749CV7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app); // Adicione esta linha
+
+export { db, app };
