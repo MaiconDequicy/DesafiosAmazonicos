13adc07a5beb34e0c30221a39ff8485c73f72145
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2025-06-27 18:53:54 -0400

    insercao de rotas de quiz e ranking

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
 import Quizzes from "./pages/quizzes";
 import AdminPage from "./pages/AdminPage";
+import QuizPage from "./pages/QuizPage"; // Importando a nova página QuizPage
+import Ranking from "./pages/Ranking";
 
 function App({ tw }) {
   return (
@@ -15,7 +17,9 @@ function App({ tw }) {
           <Route path="/cadastro" element={<Cadastro tw={tw} />} />
           <Route path="/home" element={<Home tw={tw} />} />
           <Route path="/quizzes" element={<Quizzes tw={tw} />} />
+          <Route path="/quiz/:quizId" element={<QuizPage tw={tw} />} /> {/* Nova rota para QuizPage */}
           <Route path="/admin" element={<AdminPage tw={tw} />} />
+          <Route path="/ranking" element={<Ranking tw={tw} />}></Route>
           <Route path="*" element={<Login tw={tw} />} /> // Redireciona para Login se a rota não existir
         </Routes>
       </div>
